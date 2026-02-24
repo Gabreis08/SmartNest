03b042f3f1f25e78be4e12ca8aa2bc075850dcd6
Gabriel Reis <bielreiscavm@gmail.com>
2026-02-24 08:39:39 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//Fazer uma referencia a cada botão de navegação
+const start = document.getElementById("avancar");
+const login = document.getElementsByTagName("input")[3];
+const setavoltar = document.getElementById("setavoltar");
+if(start!=null){
+start.onclick =()=>{
+    window.location.href="login.html";
+    }
+}
+if(login!=null){
+login.onclick = ()=>{
+    window.location.href="cadastro.html";
+   }
+}
+if(setavoltar!=null){
+setavoltar.onclick = ()=>{
+    window.location.href="index.html";
+    }
+}
